3a3f29072c2a5e3c2387534bd92d00f6a22bc237
nadalet102 <ferran@arisac.com>
2026-05-25 17:53:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit:'20mb'}));
+app.use(express.urlencoded({extended:true,limit:'20mb'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = new Pool({
